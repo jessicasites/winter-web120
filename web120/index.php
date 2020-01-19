1da8ca1863aebdafe8fe124f06fa35708b0b8ee2
jessicasites <56246446+jessicasites@users.noreply.github.com>
2020-01-19 00:07:26 -0800

    Add files via upload

---
+++ web120/index.php
@@ -0,0 +1,17 @@
+<?php include 'includes/header.php';?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="Picture of me at the zoo" />
+ <img src="images/phone.jpg" class="phone" alt="Picture of me in Maui" />
+ <p>I am a full-time student at Seattle Central. I am pursuing an AAS in Computer Programming. I have previously worked in the medical field, and was at my last job, as a Cardiotech for 10 years. This is quite a change for me; learning a whole new language and getting used to a different culture. I am well versed in medical terminology, so this has been an interesting transition. I sometimes feel like I’ve been plucked out of one reality and dropped into another. But, despite all the changes, and necessary adjusting, I am truly excited to be learning something new, and am looking forward to seeing where this journey will take me. 
+When I’m not at school, I enjoy spending time with friends, furry animals and communing with nature. I love a good documentary, have a passion for singing and music, my favorite television series is Black Mirror and I am obsessed with anything related to Psychology: true crime, research articles, books etc.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <img src="images/tablet.jpg" class="tablet" alt="Picture of me and my dog" />
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php';?>
